2637ef6f645a726bd057682a4dbfa2a16ad10f9c
skuuzie <138307270+skuuzie@users.noreply.github.com>
2025-01-19 14:42:38 +0700

    implement change password and logout

--- app/src/main/java/com/enigma/catat_arey/ui/setting/SettingActivity.kt
+++ app/src/main/java/com/enigma/catat_arey/ui/setting/SettingActivity.kt
@@ -1,17 +1,30 @@
 package com.enigma.catat_arey.ui.setting
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.enigma.catat_arey.R
 import com.enigma.catat_arey.databinding.ActivitySettingBinding
+import com.enigma.catat_arey.ui.home.HomeUiState
+import com.enigma.catat_arey.ui.startup.MainActivity
+import com.enigma.catat_arey.util.ErrorPopupDialog
+import com.enigma.catat_arey.util.GeneralUtil.isValidPassword
+import com.enigma.catat_arey.util.LoadingDialog
 import com.enigma.catat_arey.util.showCustomDialog
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SettingActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySettingBinding
+    private val viewModel: SettingViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -29,6 +42,30 @@ class SettingActivity : AppCompatActivity() {
         }
 
         setupListener()
+
+        viewModel.getCurrentUserData().observe(this) {
+            when (it) {
+                is HomeUiState.Error -> {
+                    setGlobalLoading(false)
+                    showToast(it.message)
+                }
+
+                HomeUiState.Loading -> {
+                    setGlobalLoading(true)
+                    binding.btnLogout.isEnabled = false
+                    binding.btnChangePw.isEnabled = false
+                }
+
+                is HomeUiState.Success -> {
+                    setGlobalLoading(false)
+                    binding.btnLogout.isEnabled = true
+                    binding.btnChangePw.isEnabled = true
+                    binding.loadingIndicator.visibility = View.INVISIBLE
+                    binding.tvGreetings.text =
+                        getString(R.string.setting_greeting, it.data!!.username)
+                }
+            }
+        }
     }
 
     private fun setupListener() {
@@ -42,20 +79,134 @@ class SettingActivity : AppCompatActivity() {
                 title = "Ubah Password",
                 layoutId = R.layout.dialog_change_pw_layout,
                 onPositiveAction = { dialogView, dialog ->
+                    val invalidPw =
+                        "Password harus terdiri dari 8-30 karakter dan menyertakan setidaknya satu huruf besar, satu huruf kecil, satu angka, dan satu karakter khusus (!@#\$%^&*)"
+
                     val edCurrPw = dialogView.findViewById<EditText>(R.id.ed_current_pw)
                     val edNewPw = dialogView.findViewById<EditText>(R.id.ed_new_pw)
                     val edConfPw = dialogView.findViewById<EditText>(R.id.ed_confirm_pw)
 
-                    // TODO: Add change password logic here
-                    if (edNewPw.text.toString() == edConfPw.text.toString()) {
-                        // update pw
-                        dialog.setCancelable(true)
-                        dialog.dismiss()
-                    } else {
-                        edConfPw.error = "Password tidak sama"
+                    val currPw = edCurrPw.text.toString()
+                    val newPw = edNewPw.text.toString()
+
+                    if (edCurrPw.validatePasswordInput(invalidPw) &&
+                        edNewPw.validatePasswordInput(invalidPw) &&
+                        edConfPw.validatePasswordInput(
+                            invalidPw,
+                            isMatchCheck = true,
+                            matchValue = newPw
+                        )
+                    ) {
+                        viewModel.updateUserData(currPw, newPw).observe(this) {
+                            when (it) {
+                                is SettingUiState.Error -> {
+                                    setGlobalLoading(false)
+
+                                    val msg = it.message
+
+                                    if (msg.lowercase().contains("network error")) {
+                                        showNetworkError { }
+                                    } else {
+                                        showToast("Error: ${it.message}")
+                                    }
+                                }
+
+                                SettingUiState.Loading -> {
+                                    setGlobalLoading(true)
+                                }
+
+                                is SettingUiState.Success -> {
+                                    setGlobalLoading(false)
+                                    showToast("Password berhasil diganti.")
+                                    viewModel.wipeUserData()
+                                    dialog.dismiss()
+                                    moveToLogin()
+                                }
+                            }
+                        }
                     }
                 }
             )
+        }
+
+        binding.btnLogout.setOnClickListener {
+            viewModel.wipeUserData().observe(this) {
+                when (it) {
+                    is SettingUiState.Error -> {
+                        setGlobalLoading(false)
+
+                        val msg = it.message
+
+                        if (msg.lowercase().contains("network error")) {
+                            showNetworkError { }
+                        } else {
+                            showToast("Error: ${it.message}")
+                        }
+                    }
+
+                    SettingUiState.Loading -> {
+                        setGlobalLoading(true)
+                    }
+
+                    is SettingUiState.Success -> {
+                        setGlobalLoading(false)
+                        showToast("Berhasil Log Out.")
+                        moveToLogin()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun EditText.validatePasswordInput(
+        errorMessage: String,
+        isMatchCheck: Boolean = false,
+        matchValue: String? = null
+    ): Boolean {
+        error = when {
+            text.isNullOrEmpty() -> {
+                "Field ini tidak boleh kosong"
+            }
+
+            !isValidPassword(text.toString()) -> {
+                errorMessage
+            }
+
+            isMatchCheck && text.toString() != matchValue -> {
+                "Password harus sama"
+            }
+
+            else -> null
+        }
+        return error == null
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun moveToLogin() {
+        Intent(this, MainActivity::class.java).run {
+            startActivity(this)
+            finishAffinity()
+        }
+    }
+
+    private fun showNetworkError(onButtonClick: () -> Unit) {
+        ErrorPopupDialog.showError(
+            context = this,
+            title = "Kesalahan Jaringan",
+            message = "Silahkan coba lagi atau hubungi administrator jika berkelanjutan.",
+            buttonText = "Coba Lagi",
+            onButtonClick = onButtonClick
+        )
+    }
+
+    private fun setGlobalLoading(state: Boolean) {
+        if (state) {
+            LoadingDialog.showLoading(this)
+        } else {
+            LoadingDialog.hideLoading()
         }
     }
 }
---
+++ app/src/main/java/com/enigma/catat_arey/ui/setting/SettingViewModel.kt
@@ -0,0 +1,70 @@
+package com.enigma.catat_arey.ui.setting
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.enigma.catat_arey.data.network.NetworkRepository
+import com.enigma.catat_arey.data.network.ResponseResult
+import com.enigma.catat_arey.data.network.UserDataResponse
+import com.enigma.catat_arey.data.preferences.DatastoreManager
+import com.enigma.catat_arey.ui.home.HomeUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingViewModel @Inject constructor(
+    private val datastoreManager: DatastoreManager,
+    private val networkRepository: NetworkRepository
+) : ViewModel() {
+
+    /*
+        Used when activity startup and retries
+     */
+    fun getCurrentUserData(): LiveData<HomeUiState<UserDataResponse>> = liveData {
+        emit(HomeUiState.Loading)
+
+        when (val resp = networkRepository.getUserData(datastoreManager.currentUserId.first())) {
+            is ResponseResult.Error -> emit(HomeUiState.Error(resp.message))
+            is ResponseResult.Success -> emit(HomeUiState.Success(resp.data))
+        }
+    }
+
+    /*
+        Used when user wants to change password
+     */
+    fun updateUserData(
+        currentPassword: String,
+        newPassword: String
+    ): LiveData<SettingUiState<Nothing>> = liveData {
+        emit(SettingUiState.Loading)
+        when (val resp = networkRepository.updateUser(
+            datastoreManager.currentUserId.first(),
+            currentPassword,
+            newPassword
+        )) {
+            is ResponseResult.Error -> {
+                emit(SettingUiState.Error(resp.message))
+            }
+
+            is ResponseResult.Success -> {
+                emit(SettingUiState.Success(null))
+            }
+        }
+    }
+
+    /*
+        Used when user logs out
+     */
+    fun wipeUserData(): LiveData<SettingUiState<Nothing>> = liveData {
+        emit(SettingUiState.Loading)
+        datastoreManager.cleanup()
+        emit(SettingUiState.Success(null))
+    }
+}
+
+sealed interface SettingUiState<out T> {
+    data object Loading : SettingUiState<Nothing>
+    data class Error(val message: String) : SettingUiState<Nothing>
+    data class Success<T>(val data: T?) : SettingUiState<T>
+}
